457c75fe50185abc2b2da6f91b798a3e9e7b9f51
elif inanc <elifinanc2015@gmail.com>
2020-04-06 11:30:20 +0200

    first commit

---
+++ Quete1-MEF-InjectionDeDependance/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+
+namespace Quete1_MEF_InjectionDeDependance
+{
+    class Program
+    {
+        private Host _host;
+
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Run();
+
+            Console.ReadKey();
+        }
+
+        private void Run()
+        {
+            _host = new Host();
+            HelloSayer service = _host.Container.GetExportedValue<HelloSayer>();
+        }
+    }
+
+    internal class Host
+    {
+        public CompositionContainer Container
+        {
+            get
+            {
+                if (_container == null)
+                {
+                    var catalog = new DirectoryCatalog(".", "Quete1_MEF_InjectionDeDependance.*");
+                    _container = new CompositionContainer(catalog);
+                }
+                return _container;
+            }
+        }
+        private CompositionContainer _container = null;
+    }
+
+    [Export]
+    internal class SayHello
+    {
+        public SayHello()
+        {
+            Console.WriteLine("Hello !");
+        }
+    }
+
+    [Export]
+    internal class HelloSayer
+    {
+        [Import]
+        private SayHello sayHello;
+    }
+}
+
